7af38a6264a8f374df4863cedd1972beddfca9c7
JungHyundoo <jhd0817@gmail.com>
2024-03-27 17:44:43 +0900

    Initial commit

---
+++ starcom/src/main/java/com/sc/starcom/StarcomApplication.java
@@ -0,0 +1,13 @@
+package com.sc.starcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarcomApplication.class, args);
+    }
+
+}
---
+++ starcom/src/test/java/com/sc/starcom/StarcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.starcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
