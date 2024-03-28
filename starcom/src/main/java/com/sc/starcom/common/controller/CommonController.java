ae996b5d26b52ffbccd6af2e0bab876605df90a1
cjswjddls <68465766+cjswjddls@users.noreply.github.com>
2024-03-29 03:05:36 +0900

    메인으로 포워딩

--- starcom/src/main/java/com/sc/starcom/common/controller/CommonController.java
+++ starcom/src/main/java/com/sc/starcom/common/controller/CommonController.java
@@ -1,10 +1,10 @@
 package com.sc.starcom.common.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CommonController {
-
 
 }
