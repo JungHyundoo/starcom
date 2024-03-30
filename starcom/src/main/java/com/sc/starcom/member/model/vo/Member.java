7234d7a75145c327db50e71d8d418edc353c3d2a
JungHyundoo <jhd0817@gmail.com>
2024-03-30 15:48:54 +0900

    메인으로 포워딩

--- starcom/src/main/java/com/sc/starcom/member/model/vo/Member.java
+++ starcom/src/main/java/com/sc/starcom/member/model/vo/Member.java
@@ -1,18 +1,26 @@
 package com.sc.starcom.member.model.vo;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
+import java.sql.Date;
+
+
+@NoArgsConstructor
 @Getter
 @Setter
 @AllArgsConstructor
 @ToString
 public class Member {
 
-    private int memberNo; // 회원번호
-    private String memberName; // 회원이름
-    private String memberNick; // 회원닉네임
+    private int userNo; // 회원 번호
+    private String userId; // 회원 아이디
+    private String userPwd; // 회원 비밀번호
+    private String userName; // 회원 이름
+    private String userNick; // 회원 닉네임
+    private String userEmail; // 이메일
+    private String userPhone; // 핸드폰번호
+    private String userImg; // 프로필사진
+    private Date createDate; // 생성날짜
+    private int userStatus; // 회원 상태
 
 }
